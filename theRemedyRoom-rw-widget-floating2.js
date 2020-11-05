1f212bc7e6c03201d48e8dab0289b78704d070b4
Bryan Cancel <bryan.o.cancel@gmail.com>
2020-11-04 23:09:36 -0500

    Update theRemedyRoom-rw-widget-floating2.js

--- theRemedyRoom-rw-widget-floating2.js
+++ theRemedyRoom-rw-widget-floating2.js
@@ -1,2 +1,2 @@
 //started with DUP of https://widgets.thereviewsplace.com/2.0/rw-widget-floating.js
-console.log("EDIT of https://widgets.thereviewsplace.com/2.0/rw-widget-floating.js");
+console.log("hello world");
